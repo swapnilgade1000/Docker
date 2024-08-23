830412d517d3f43e91c65f355533383f6ef08a91
swapnilgade100 <swapnilgade100@gmail.com>
2024-08-23 22:56:52 +0530

    --- Auto Git Commit ---

--- testing/main.tf
+++ testing/main.tf
@@ -13,3 +13,28 @@ resource "aws_instance" "this" {
 
 }
 
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.region
+}
+
+##### Try to create one ubuntu server
+resource "aws_instance" "this" {
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  key_name                = var.key_name
+
+  tags = var.tags
+
+}
+
