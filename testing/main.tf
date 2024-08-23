4e3aafc2ee80f60467d96bffec26a145b4bd3211
swapnilgade100 <swapnilgade100@gmail.com>
2024-08-23 23:04:24 +0530

    --- Auto Git Commit ---

--- testing/main.tf
+++ testing/main.tf
@@ -1,15 +1,5 @@
 
 
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.61.0"
-    }
-  }
-}
-
 provider "aws" {
   # Configuration options
   region = "ap-south-1"
