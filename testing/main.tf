bf6de3aca1c0dd113546ce2e09d98f960a49402f
swapnilgade100 <swapnilgade100@gmail.com>
2024-08-23 18:50:25 +0530

    --- Auto Git Commit ---

--- testing/main.tf
+++ testing/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "this" {
   ami                     = ami-0522ab6e1ddcc7055
   instance_type           = t2.micro
-  key_name                = v
+  key_name                = MyAWSKey
 
   tags = var.tags
 
