c7d629bde2c77e7dce7b2d4de44185ad569648a0
swapnilgade100 <swapnilgade100@gmail.com>
2024-08-23 19:27:54 +0530

    --- Auto Git Commit ---

--- testing/main.tf
+++ testing/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 ##### Try to create one ubuntu server
 resource "aws_instance" "this" {
   ami                     = "ami-0522ab6e1ddcc7055"
-  instance_type           = t2.micro
+  instance_type           = "t2.micro"
   key_name                = MyAWSKey
 
   tags = var.tags
