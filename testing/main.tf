0bcb03902fa1d3ea9027fb59cf6fbd7bbf0d3153
swapnilgade100 <swapnilgade100@gmail.com>
2024-08-23 22:59:24 +0530

    --- Auto Git Commit ---

--- testing/main.tf
+++ testing/main.tf
@@ -1,17 +1,4 @@
-provider "aws" {
-  # Configuration options
-  region = "ap-south-1"
-}
 
-##### Try to create one ubuntu server
-resource "aws_instance" "this" {
-  ami                     = "ami-0522ab6e1ddcc7055"
-  instance_type           = "t2.micro"
-  key_name                = "MyAWSKey"
-
-  tags = var.tags
-
-}
 
 
 terraform {
