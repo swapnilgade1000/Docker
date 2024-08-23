98db39a0ab09faed6402af7d9b063fd4768477b0
swapnilgade100 <swapnilgade100@gmail.com>
2024-08-23 18:35:30 +0530

    --- Auto Git Commit ---

--- Github TF/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "github" {
-    token = "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"
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 479c4c796e78011c177cb0b5fb9f26aec7c247e0
--- Github TF/resourses.tf
+++
@@ -1,6 +0,0 @@
-resource "github_repository" "my_first_repo" {
-    name        = "first-tf-repo"
-    description = "My awesome codebase"
-    visibility = "public"
-    auto_init = true
-}
